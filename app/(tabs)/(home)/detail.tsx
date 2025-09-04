8392ab4dafad426d3bc689028d32d5bb9f1266c3
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-04 23:39:08 +0900

    router.replace screen

--- app/(tabs)/(home)/detail.tsx
+++ app/(tabs)/(home)/detail.tsx
@@ -1,13 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { Link, useLocalSearchParams, useRouter } from "expo-router";
 
 export default function Detail() {
   const { id } = useLocalSearchParams();
+ const router = useRouter()
 
   return (
     <View>
       <Text>Detail page - {id}</Text>
+      <Link href='/event'>Navigate Event</Link>
+      <Link href='/event' replace>Replace Event</Link>
+      <Button title="Navigate Event" onPress={()=> router.navigate('/event')} />
+      <Button title="Replace Home" onPress={()=> router.replace('/')} />
     </View>
   );
 }
