a192466f5700c55cea5ebb9611b7131ee7dfe3cd
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-04 21:09:52 +0900

    navigation with useRouter

--- app/(tabs)/(home)/detail.tsx
+++ app/(tabs)/(home)/detail.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
 
 export default function Detail() {
+  const { id } = useLocalSearchParams();
+
   return (
     <View>
-      <Text>Detail page</Text>
+      <Text>Detail page - {id}</Text>
     </View>
-  )
+  );
 }
--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,37 +1,13 @@
-import { Link } from "expo-router";
-import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { StyleSheet, View, Button } from "react-native";
 
-type StaffProps = { name: string; age?: number };
-
-const Staff = ({ name, age }: StaffProps) => {
-  const [isDelete, setIsDelete] = useState(false);
-
-  return (
-    <>
-      <Text style={{ textDecorationLine: isDelete ? "line-through" : "none" }}>
-        {name}
-        {age ? " - " + age : ""}
-      </Text>
-      <Button
-        title={isDelete ? "undo" : "delete"}
-        onPress={() => setIsDelete(!isDelete)}
-      />
-    </>
-  );
-};
 export default function HomeScreen() {
-  const greeting = "Hello Mobile Developer";
+  const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Text>{greeting}</Text>
-      <Link href="/detail">Go to detail</Link>
-      <Link href="/profile/setting">Go to Profile Setting</Link>
-      <Link href="/profile">Go to Profile</Link>
-      <Staff name="koko" age={25} />
-      <Staff name="nyi nyi" />
-      <Staff name="ma ma" />
+      <Link href="/detail?id=5">Go to Home detail</Link>
+      <Button title="Go to Detail" onPress={() => router.navigate({pathname: '/detail', params: {id: 7}})} />
     </View>
   );
 }
