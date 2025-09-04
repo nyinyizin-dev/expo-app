fc03e9a69a5bd05ce37f773cda17e8b17e1ae2e4
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-05 00:09:54 +0900

    router.push & still screen & change params

--- app/(tabs)/(home)/detail.tsx
+++ app/(tabs)/(home)/detail.tsx
@@ -11,8 +11,12 @@ export default function Detail() {
       <Text>Detail page - {id}</Text>
       <Link href='/event'>Navigate Event</Link>
       <Link href='/event' replace>Replace Event</Link>
+      <Link href='/detail?id=another007' push>Push Another Detail</Link>
       <Button title="Navigate Event" onPress={()=> router.navigate('/event')} />
       <Button title="Replace Home" onPress={()=> router.replace('/')} />
+      <Button title="Push another Detail" onPress={()=> router.push({pathname:'/detail',params: {id:'another009'}})} />
+      <Button title="Still Detail with Route" onPress={() => router.navigate({pathname: '/detail', params: {id: 'change-params'}})} />
+      <Button title="Still Detail with setParams" onPress={() => router.setParams({id: 'change-value'})} />
     </View>
   );
 }
