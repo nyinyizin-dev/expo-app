587e45572202cfa081b3318ae7cd8fb4389628d5
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-04 22:34:53 +0900

    Configure Header Title & set Dynamically

--- app/(tabs)/(home,profile)/settings/[username].tsx
+++ app/(tabs)/(home,profile)/settings/[username].tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { useLocalSearchParams, Stack, useRouter } from "expo-router";
 
 export default function Setting() {
-  const {username} = useLocalSearchParams();
+  const { username } = useLocalSearchParams();
+//   const navigation = useNavigation();
+
+//   useEffect(() => {
+//     navigation.setOptions({
+//       title: username as string,
+//     });
+//   }, [navigation]);
+
+    const router = useRouter()
+
   return (
     <View>
+      <Stack.Screen options={{title: username as string }} />
       <Text>User - {username}</Text>
+      <Button title="Update title" onPress={() => router.setParams({username: 'Updated name'})}/>
     </View>
-  )
+  );
 }
