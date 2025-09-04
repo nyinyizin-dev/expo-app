c1ad3bdab8b7a23a510ed3a73a7b793144b52eb2
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-04 20:14:10 +0900

    profile Tab Icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,8 +24,6 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="(setting)" options={{ title: "Setting" }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
