360e6f045d4ce161e0c0bb43e82fdf96211898a0
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-05 20:26:22 +0900

    Secure store - persistent data

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,11 +1,18 @@
 import { View, Text, TextInput,Button } from "react-native";
 import React, { useState } from "react";
+import * as SecureStore from 'expo-secure-store';
 import { useCounter } from "@/providers/countContext";
 
 export default function HomeScreen() {
-  const [text, setText] = useState("");
-  
- const {count, increase, decrease} = useCounter();
+  const [text, setText] = useState(""); 
+  const {count, increase, decrease} = useCounter();
+
+  const saveToSecureStore = async () => {
+    await SecureStore.setItemAsync('name',text)
+  }
+  const deleteFromSecureStore = async () => {
+    await SecureStore.deleteItemAsync('name')
+  }
 
   return (
     <View>
@@ -16,6 +23,8 @@ export default function HomeScreen() {
         onChangeText={(text) => setText(text)}
         defaultValue={text}
       />
+      <Button title="Save to secure store" onPress={saveToSecureStore} />
+      <Button title="Delete name from secure store" onPress={deleteFromSecureStore} />
       <Text style={{ fontSize: 32, marginTop: 10}}>Count : {count}</Text>
       <Button title="Increase + " onPress={increase} />
       <Button title="Decrease + " onPress={decrease} />
--- app/(tabs)/(profile)/index.tsx
+++ app/(tabs)/(profile)/index.tsx
@@ -1,14 +1,23 @@
 import { View, Text, Button } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "expo-router";
+import * as SecureStore from "expo-secure-store";
 import { useCounter } from "@/providers/countContext";
 
 export default function Profile() {
   const { count, increase, decrease } = useCounter();
+  const [show, setShow] = useState("");
+
+  const getFromSecureStore = async () => {
+    const name = await SecureStore.getItemAsync("name");
+    setShow(name || "No name found");
+  };
 
   return (
     <View>
       <Text>Profile Screen</Text>
+      <Text style={{ fontSize: 27, marginTop: 10 }}>Name : {show}</Text>
+      <Button title="Get data from secure store" onPress={getFromSecureStore} />
       <Link href="/settings/David">Go to Share Setting</Link>
       <Link href={"/detail"}>Go to Home Detail</Link>
       <Link href={"/"}>Go to Home </Link>
