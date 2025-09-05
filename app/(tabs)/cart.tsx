0ba72a82023a42bd893901cff9e5d1f012b89c28
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-05 15:46:18 +0900

    googleFont with expo-font

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -14,8 +14,10 @@ export default function Cart() {
       <CustomText style={{ fontSize: 30 }}>Welcome Cart Screen</CustomText>
       <CustomText style={{ fontFamily: "NotoSerifJP" }}>おはいよいよ</CustomText>
       <CustomText style={{ fontFamily: "Oswald-Bold" }}>Cart Screen</CustomText>
-      <CustomText style={{ fontFamily: "Inter_500Medium_Italic" }}>Inter_500Medium_Italic</CustomText>
-      <CustomText style={{ fontFamily: "Inter_900Black" }}>Inter_900Black</CustomText>
+      <Text style={{ fontSize: 30,fontFamily: "SpaceMono" }}>Space Mono</Text>
+      <Text style={{ fontSize: 30}}>System Font</Text>
+      {/* <CustomText style={{ fontFamily: "Inter_500Medium_Italic" }}>Inter_500Medium_Italic</CustomText>
+      <CustomText style={{ fontFamily: "Inter_900Black" }}>Inter_900Black</CustomText> */}
     </View>
   );
 }
