d51cb01b20a4e400f16a4e8d4f4395b18040f47a
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-05 16:38:21 +0900

    Image

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,5 +1,5 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { Text, View,ScrollView} from "react-native";
+import { Image } from "expo-image";
 import {
   SafeAreaView,
   useSafeAreaInsets,
@@ -11,13 +11,23 @@ export default function Cart() {
 
   return (
     <View style={{ flex: 1, paddingTop: insets.top }}>
+      <ScrollView>
       <CustomText style={{ fontSize: 30 }}>Welcome Cart Screen</CustomText>
       <CustomText style={{ fontFamily: "NotoSerifJP" }}>おはいよいよ</CustomText>
       <CustomText style={{ fontFamily: "Oswald-Bold" }}>Cart Screen</CustomText>
       <Text style={{ fontSize: 30,fontFamily: "SpaceMono" }}>Space Mono</Text>
-      <Text style={{ fontSize: 30}}>System Font</Text>
-      {/* <CustomText style={{ fontFamily: "Inter_500Medium_Italic" }}>Inter_500Medium_Italic</CustomText>
-      <CustomText style={{ fontFamily: "Inter_900Black" }}>Inter_900Black</CustomText> */}
+      <Text style={{ fontSize: 30}}>System </Text>
+      <CustomText style={{ fontFamily: "Inter_500Medium_Italic" }}>Inter_500Medium_Italic</CustomText>
+      <CustomText style={{ fontFamily: "Inter_900Black" }}>Inter_900Black</CustomText>
+
+      <Image source={require('@/assets/images/react-logo.png')} style={{width: 150, height: 150, }} />
+      <Image source={{uri:'https://i.pravatar.cc/400?img=37'}} 
+      style={{width: 150, height: 200, resizeMode: 'contain',backgroundColor: "teal"}} />
+      <Image source={{uri:'man'}} 
+      style={{width: 150, height: 200, resizeMode: 'contain',backgroundColor: "teal"}} />
+     
+
+      </ScrollView> 
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,28 +7,28 @@ import {
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-// import {
-//   Inter_500Medium_Italic,
-//   useFonts,
-//   Inter_900Black,
-// } from "@expo-google-fonts/inter";
+import {
+  Inter_500Medium_Italic,
+  useFonts,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  // const [loaded] = useFonts({
-  //   SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  //   NotoSerifJP: require("../assets/fonts/NotoSerifJP-Regular.ttf"),
-  //   "Oswald-Bold": require("../assets/fonts/Oswald-Bold.ttf"),
-  //   Inter_900Black,
-  //   Inter_500Medium_Italic,
-  // });
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    NotoSerifJP: require("../assets/fonts/NotoSerifJP-Regular.ttf"),
+    "Oswald-Bold": require("../assets/fonts/Oswald-Bold.ttf"),
+    Inter_900Black,
+    Inter_500Medium_Italic,
+  });
 
-  // if (!loaded) {
-  //   // Async font loading only occurs in development.
-  //   return null;
-  // }
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
